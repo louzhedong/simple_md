941da74a68cba2f5f7f14dcc76ddc7a198789fb2
楼哲东 <louzhedong@bytedance.com>
2022-03-18 23:01:25 +0800

    feat: 添加微信二维码

--- frontend/src/pages/MainPage/components/UserInfo.vue
+++ frontend/src/pages/MainPage/components/UserInfo.vue
@@ -3,29 +3,50 @@
     <div class="user-avatar">
       <img :src="avatar" alt="" class="avatar-img" />
     </div>
-		<h3 class="user-nickname">Miracle</h3>
-		<div class="social-icon">
-			<a class="iconfont icon-github" href="https://github.com/louzhedong" target="__blank"></a>
-			<i class="iconfont icon-weixin"></i>
-		</div>
+    <h3 class="user-nickname">Miracle</h3>
+    <div class="social-icon">
+      <a
+        class="iconfont icon-github"
+        href="https://github.com/louzhedong"
+        target="__blank"
+      ></a>
+      <i
+        class="iconfont icon-weixin"
+        @mouseover="handleShowWechat"
+        @mouseleave="handleHideWechat"
+      >
+        <img :src="wechat" alt="" class="wechat" v-if="showWechat" />
+      </i>
+    </div>
   </div>
 </template>
 
 <script>
 import avatar from "@assets/cat.jpeg";
+import wechat from "@assets/wechat.png";
 
 export default {
   data() {
     return {
       avatar,
+      wechat,
+      showWechat: false,
     };
+  },
+  methods: {
+    handleShowWechat() {
+      this.showWechat = true;
+    },
+    handleHideWechat() {
+      this.showWechat = false;
+    },
   },
 };
 </script>
 
 <style lang="less">
 .user-info {
-	width: 150px;
+  width: 150px;
   .user-avatar {
     width: 150px;
     height: 150px;
@@ -36,18 +57,27 @@ export default {
       height: 150px;
     }
   }
-	.user-nickname {
-		font-size: 30px;
-		margin-top: 20px;
-		text-align: center;
-	}
-	.social-icon {
-		.iconfont {
-			font-size: 24px;
-			margin: 4px;
-			color: #777;
-		}
-	}
+  .user-nickname {
+    font-size: 30px;
+    margin-top: 20px;
+    text-align: center;
+  }
+  .social-icon {
+    .iconfont {
+      font-size: 24px;
+      margin: 4px;
+      color: #777;
+      position: relative;
+    }
+    .wechat {
+      width: 150px;
+      height: 150px;
+      border-radius: 10px;
+      position: absolute;
+      bottom: 20px;
+      left: 10px;
+    }
+  }
 }
 </style>
 
