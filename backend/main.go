14ee20739581913cc2006d7439cea1a84a506641
楼哲东 <louzhedong@bytedance.com>
2022-03-29 16:56:42 +0800

    feat: 添加一个markdown文件

--- backend/main.go
+++ backend/main.go
@@ -9,6 +9,21 @@ import (
 	"net/http"
 )
 
+func cors(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")  // 允许访问所有域，可以换成具体url，注意仅具体url才能带cookie信息
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token") //header的类型
+		w.Header().Add("Access-Control-Allow-Credentials", "true") //设置为true，允许ajax异步请求带cookie信息
+		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE") //允许请求方法
+		w.Header().Set("content-type", "application/json;charset=UTF-8")             //返回数据格式是json
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		f(w, r)
+	}
+}
+
 func sayHello(w http.ResponseWriter, r *http.Request)  {
 	fmt.Println("hello")
 }
@@ -27,7 +42,7 @@ func mdDemo(w http.ResponseWriter, r *http.Request) {
 
 func main()  {
 	http.HandleFunc("/", sayHello)
-	http.HandleFunc("/md", mdDemo)
+	http.HandleFunc("/md", cors(mdDemo))
 	fmt.Println("Listen port 9090")
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
